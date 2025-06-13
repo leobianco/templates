fecfe17614806982a5dc1f78a9411e664ac6eb5d
Leonardo Martins Bianco <leonardo@rhl.com.br>
2025-06-13 16:11:53 +0200

    git aliases

---
+++ git_aliases.sh
@@ -0,0 +1,6 @@
+git config --global alias.ga add
+git config --global alias.gc commit
+git config --global alias.gp push
+git config --global alias.co checkout
+git config --global alias.br branch
+git config --global alias.st status
